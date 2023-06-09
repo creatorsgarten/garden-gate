dd73a81fa30762e55082007c479b9fd321dd3ac4
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-10 01:22:44 +0700

    implement most important simulator methods

--- src/simulator.ts
+++ src/simulator.ts
@@ -5,6 +5,14 @@ import {
   buildWWWAuthenticateHeader,
 } from "http-auth-utils";
 
+/*
+To test with the simulator, use the following environment variables:
+
+HIKVISION_IP=127.0.0.1:3331
+HIKVISION_EMPLOYEE_NO=500033
+HIKVISION_ADMIN_PASSWORD=simulatorPassword
+*/
+
 function digestAuth(request: Request): any {
   const realm = "DS-SIMULATOR";
   const qop = "auth";
@@ -18,7 +26,7 @@ function digestAuth(request: Request): any {
     qop,
     realm,
     username: result.data.username,
-    password: "wtf",
+    password: "simulatorPassword",
     uri: result.data.uri,
     nonce: result.data.nonce,
     nc: result.data.nc || "",
@@ -43,20 +51,40 @@ function digestAuth(request: Request): any {
   }
 }
 
+function badRequest(message: string): any {
+  return new Response(`[simulator: bad request] ${message}`, {
+    status: 400,
+  }) as any;
+}
+
+interface CardInfo {
+  employeeNo: string;
+  cardNo: string;
+  leaderCard: string;
+  cardType: string;
+}
+
+const cards: Map<string, CardInfo> = new Map();
+
 new Elysia()
   .post(
     "/ISAPI/AccessControl/CardInfo/Search",
     async ({ body, request }) => {
       const unauthorized = digestAuth(request);
       if (unauthorized) return unauthorized;
-      console.log(body);
+      const found = Array.from(cards.values()).filter((card) =>
+        body.CardInfoSearchCond.EmployeeNoList.some(
+          (employee) => employee.employeeNo === card.employeeNo
+        )
+      );
+      console.log(`[Search] Found ${found.length} cards`);
       return {
         CardInfoSearch: {
           searchID: body.CardInfoSearchCond.searchID,
           responseStatusStrg: "OK",
-          numOfMatches: 0,
-          totalMatches: 0,
-          CardInfo: [],
+          numOfMatches: found.length,
+          totalMatches: found.length,
+          CardInfo: found,
         },
       };
     },
@@ -73,22 +101,78 @@ new Elysia()
           ),
         }),
       }),
-      response: {
-        CardInfoSearch: t.Object({
-          searchID: t.String(),
-          responseStatusStrg: t.String(),
-          numOfMatches: t.Number(),
-          totalMatches: t.Number(),
-          CardInfo: t.Array(
+    }
+  )
+  .post(
+    "/ISAPI/AccessControl/CardInfo/Record",
+    async ({ body, request }) => {
+      const unauthorized = digestAuth(request);
+      if (unauthorized) return unauthorized;
+      const cardNoRegex = /^[0-9a-zA-Z-]{1,20}$/;
+      if (!cardNoRegex.test(body.CardInfo.cardNo)) {
+        return badRequest(`invalid cardNo (must match ${cardNoRegex})`);
+      }
+      if (cards.has(body.CardInfo.cardNo)) {
+        return new Response(
+          `[simulator] cardNo ${body.CardInfo.cardNo} already exists`,
+          { status: 409 }
+        );
+      }
+      cards.set(body.CardInfo.cardNo, {
+        employeeNo: body.CardInfo.employeeNo,
+        cardNo: body.CardInfo.cardNo,
+        leaderCard: "",
+        cardType: body.CardInfo.cardType,
+      });
+      console.log(
+        `[Record] Added card ${body.CardInfo.cardNo} for employee ${body.CardInfo.employeeNo}`
+      );
+      return {
+        statusCode: 1,
+        statusString: "OK",
+        subStatusCode: "ok",
+      };
+    },
+    {
+      body: t.Object({
+        CardInfo: t.Object({
+          employeeNo: t.String(),
+          cardNo: t.String(),
+          cardType: t.Literal("normalCard"),
+        }),
+      }),
+    }
+  )
+  .put(
+    "/ISAPI/AccessControl/CardInfo/Delete",
+    async ({ body, request }) => {
+      const unauthorized = digestAuth(request);
+      if (unauthorized) return unauthorized;
+      for (const cardNo of body.CardInfoDelCond.CardNoList) {
+        if (!cards.has(cardNo.cardNo)) {
+          console.log(`[Delete] Card ${cardNo.cardNo} not found`);
+          continue;
+        }
+        cards.delete(cardNo.cardNo);
+        console.log(`[Delete] Deleted card ${cardNo.cardNo}`);
+      }
+      return {
+        statusCode: 1,
+        statusString: "OK",
+        subStatusCode: "ok",
+      };
+    },
+    {
+      body: t.Object({
+        CardInfoDelCond: t.Object({
+          CardNoList: t.Array(
             t.Object({
-              employeeNo: t.String(),
               cardNo: t.String(),
-              leaderCard: t.String(),
-              cardType: t.String(),
             })
           ),
         }),
-      },
+      }),
     }
   )
-  .listen(3001);
+  // TODO: Implement POST /ISAPI/AccessControl/AcsEvent
+  .listen(3331);
